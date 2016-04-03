cf3eac7c104fe25a5a21b5a569eea83654f9800f
MariusBordeian <m.bordeian@gmail.com>
2016-04-03 20:48:20 +0300

    dns lookup client

---
+++ dnsClient.py
@@ -0,0 +1,56 @@
+from dnslib import *
+import dns.resolver
+import binascii
+import sys
+
+
+def hexPretty(hexBytes):
+	hexStr = binascii.hexlify(hexBytes)
+	prettyStr = ""
+	for i in range(1, len(hexStr)):
+		prettyStr += hexStr[i]
+		if i % 2 == 0:
+			prettyStr += ' '
+		if i % 8 == 0:
+			prettyStr += '\n'
+	return prettyStr
+
+
+def main(argv):
+	dns_server = dns.resolver.Resolver().nameservers[0]
+
+	if len(argv) < 1:
+		argv = ['www.google.com']
+
+	for a in argv:
+		print('\n\n\tDNS Lookup for : ' + a + '\n')
+
+		question = DNSRecord.question(a, "A")
+		request = question.send(dns_server)
+		response = DNSRecord.parse(request)
+
+		print('\tquestion header : ')
+		print(str(question.header) + '\n')
+		print('\tresponse header : ')
+		print(str(response.header) + '\n')
+		print('\n\t:questions')
+		print(str(response.questions) + '\n')
+		print('\n\t:answers')
+		print(str(response.rr) + '\n')
+
+		cnt = 0
+		for r in response.rr:
+			cnt += 1
+			print('answer[' + str(cnt) + ']' + '\t: ' + str(r))
+
+		if len(argv) == 1:
+			print('\n\t\t:question bytes\n' + hexPretty(question.pack()) + '\n')
+			print('\t\t:answer bytes\n' + hexPretty(request) + '\n')
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
+"""
+	https://www.wikiwand.com/en/List_of_DNS_record_types#/Resource_records
+"""
